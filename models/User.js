787ecc3bd8aba818adcf8ca5c4fc2a6da91c76ac
unknown <eilaylevi95@gmail.com>
2025-07-20 02:14:24 +0300

    [create] - create a first userModel

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Schema = require('mongoose').Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: false, // Passwords not required when user sign up with Google
+    },
+    signupwithgoogle: {
+        type: Boolean,
+        required: true,
+        default: false, // Default to false, true if user signs up with Google
+    },
+    phone:{
+        type: String,
+        required: false,   // Not required when user sign up with Google
+        
+    },
+    fullname: {
+        type: String,
+        required: false, // Not required when user sign up with Google
+    },
+    userSurvey: [
+        {
+            question: String,
+            answer: String
+        }
+    ],
+    }, {
+    timestamps: true, // Only beacuse monday.com is data-driven application
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
